ef8cae34667e773f9ea6ff289314b86448a8d217
iarroyo <iarroyoescobar@gmail.com>
2026-01-26 16:18:12 +0100

    fix(tests): update deprecation-workflow to v4 and setup intl translations
    - Upgrade ember-cli-deprecation-workflow to v4 for Vite compatibility
    - Add intl translations setup in test helpers for all test types

--- tests/helpers/index.ts
+++ tests/helpers/index.ts
@@ -4,40 +4,37 @@ import {
   setupTest as upstreamSetupTest,
   type SetupTestOptions,
 } from 'ember-qunit';
+import type { TestContext } from '@ember/test-helpers';
+import translationsForEn from 'virtual:ember-intl/translations/en';
+import translationsForEs from 'virtual:ember-intl/translations/es';
+import type IntlService from 'ember-intl/services/intl';
 
 // This file exists to provide wrappers around ember-qunit's
 // test setup functions. This way, you can easily extend the setup that is
 // needed per test type.
 
+function setupIntlTranslations(hooks: NestedHooks) {
+  hooks.beforeEach(function (this: TestContext) {
+    const intl = this.owner.lookup('service:intl') as unknown as IntlService;
+    intl.addTranslations('en', translationsForEn);
+    intl.addTranslations('es', translationsForEs);
+    intl.setLocale(['en']);
+  });
+}
+
 function setupApplicationTest(hooks: NestedHooks, options?: SetupTestOptions) {
   upstreamSetupApplicationTest(hooks, options);
-
-  // Additional setup for application tests can be done here.
-  //
-  // For example, if you need an authenticated session for each
-  // application test, you could do:
-  //
-  // hooks.beforeEach(async function () {
-  //   await authenticateSession(); // ember-simple-auth
-  // });
-  //
-  // This is also a good place to call test setup functions coming
-  // from other addons:
-  //
-  // setupIntl(hooks, 'en-us'); // ember-intl
-  // setupMirage(hooks); // ember-cli-mirage
+  setupIntlTranslations(hooks);
 }
 
 function setupRenderingTest(hooks: NestedHooks, options?: SetupTestOptions) {
   upstreamSetupRenderingTest(hooks, options);
-
-  // Additional setup for rendering tests can be done here.
+  setupIntlTranslations(hooks);
 }
 
 function setupTest(hooks: NestedHooks, options?: SetupTestOptions) {
   upstreamSetupTest(hooks, options);
-
-  // Additional setup for unit tests can be done here.
+  setupIntlTranslations(hooks);
 }
 
 export { setupApplicationTest, setupRenderingTest, setupTest };
